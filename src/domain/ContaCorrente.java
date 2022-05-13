dcfba0aa806680e4d823dc14cb59cd288288c4ff
erikyryan <erikyryang@gmail.com>
2022-05-13 08:30:57 -0300

    Pilares da orientação a objeto - Abstração

---
+++ src/domain/ContaCorrente.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class ContaCorrente {
+    public int agencia;
+    public int numero;
+    public double saldo;
+
+    public void sacar(){}
+
+    public void depositar(){}
+
+    public void transferir(){}
+}
